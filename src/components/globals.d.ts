ff22769bd443303129c9fdecfe3b25d2b2de8d60
Christian Ulbrich <christian.ulbrich@zalari.de>
2019-06-13 10:34:00 +0200

    basic working version of web-component for monacoeditor

---
+++ src/components/globals.d.ts
@@ -0,0 +1,5 @@
+export {}
+declare global {
+  const require: any;
+  const monaco: any;
+}
--- src/components/typescript-editor-component/typescript-editor-component.tsx
+++ src/components/typescript-editor-component/typescript-editor-component.tsx
@@ -4,7 +4,7 @@ import { format } from '../../utils/utils';
 @Component({
   tag: 'typescript-editor-component',
   styleUrl: 'typescript-editor-component.css',
-  shadow: true
+  shadow: false
 })
 export class TypescriptEditorComponent {
   /**
@@ -22,11 +22,48 @@ export class TypescriptEditorComponent {
    */
   @Prop() last: string;
 
+  private _editorHost: HTMLDivElement;
+
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
 
+  private _initializeMonaco() {
+    console.info('Initialize Monaco...');
+    require.config({ paths: { 'vs': '/vendor/monaco-editor/min/vs' } });
+  }
+
+  private _attachEditorToHostElement(hostElement: HTMLElement) {
+    console.log('Attach!');
+    require(['vs/editor/editor.main'], function () {
+      monaco.editor.create(hostElement, {
+        value: [
+          'function x() {',
+          '\tconsole.log("Hello world!");',
+          '}'
+        ].join('\n'),
+        language: 'typescript'
+      });
+    });
+  }
+
+  private _attachEditor() {
+    this._attachEditorToHostElement(this._editorHost);
+  }
+
+  componentWillLoad() {
+    // initialize monaco
+    this._initializeMonaco();
+  }
+
+  componentDidLoad() {
+    this._attachEditor();
+  }
+
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>
+      <div>Hello, World! I'm { this.getText() }</div>
+      <div ref={el => this._editorHost = el} class="editor"> </div>
+    </div>;
   }
 }
